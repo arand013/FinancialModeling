7ad6be7e4e381f11d1103688c5da263d01e8f9a2
Alexander Aranda <alexandermarkets1@gmail.com>
2024-05-15 21:47:31 -0400

    Yahoo_Financial_Charting.py

--- yahoo_finance_charting.py
+++ yahoo_finance_charting.py
@@ -7,6 +7,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
+# Import library for Statsmodels Formula(SMF)
+import statsmodels.formula.api as smf
 
 ticker = "AAPL"
 start_date = "2020-05-14"
@@ -18,3 +20,9 @@ warnings.filterwarnings('ignore')
 %matplotlib inline
 
 df.head()
+
+df.tail()
+
+df.shape
+
+df.describe()
