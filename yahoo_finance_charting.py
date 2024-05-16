f12bd86714775ea4b2a4e3ede9106f393a658cd2
Alexander Aranda <alexandermarkets1@gmail.com>
2024-05-15 21:33:33 -0400

    removed file

---
+++ yahoo_finance_charting.py
@@ -0,0 +1,20 @@
+# First PIP install all packages
+#  !pip install yfinance
+
+# import Libraries
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+
+ticker = "AAPL"
+start_date = "2020-05-14"
+end_date = "2024-05-14"
+
+df = yf.download(ticker, start=start_date, end=end_date)
+
+warnings.filterwarnings('ignore')
+%matplotlib inline
+
+df.head()
