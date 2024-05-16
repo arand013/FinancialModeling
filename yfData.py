abb3b83784b52ac6f3d2ac661d5c4b5e6c704bb1
Alexander Aranda <alexandermarkets1@gmail.com>
2024-05-15 22:53:59 -0400

    yfData.py: created: imported and created ticker call

--- yfData.py
+++ yfData.py
@@ -1,9 +1,20 @@
+# !pip install pandas
+# !pip install yfinance
 
 import warnings
 warnings.filterwarnings('ignore')
-
 import pandas as pd
 import yfinance as yf
-symbol = 'FB'
+
+symbol = 'T'
 ticker = yf.Ticker(symbol)
 pd.Series(ticker.info).head(20)
+
+data = ticker.history(period='5d',
+                      interval='1m',
+                      start=None,
+                      end=None,
+                      actions=True,
+                      auto_adjust=True,
+                      back_adjust=False)
+data.info()
